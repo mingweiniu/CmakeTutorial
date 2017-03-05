4da8ec982622729f32d28678d3d87cae56ded506
Sidney Niu <mingweiniu@gmail.com>
2017-03-05 16:58:00 +0800

    Update getBoost.cmake

--- cmake/Modules/getBoost.cmake
+++ cmake/Modules/getBoost.cmake
@@ -1,12 +1,19 @@
 
-if(WiN32)
-	add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
-endif()
-
-message("setting boost")
+message("setting boost multithread/static_libs/static_runtime")
+SET(Boost_DEBUG ON) # Set to ON to enable debug output from FindBoost.
+SET(Boost_DETAILED_FAILURE_MSG ON)
 SET(Boost_USE_MULTITHREADED ON)
 SET(Boost_USE_STATIC_LIBS ON)
 SET(Boost_USE_STATIC_RUNTIME ON)
-SET(Boost_DEBUG ON)
+
+
+if(WiN32)
+	add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
+	find_path(BOOST_ROOT NAMES boost/version.hpp HINTS
+		"$ENV{BOOST_ROOT}"
+		"$ENV{BOOSTROOT}"
+	)
+endif()
+
 message("find_package(Boost)")
 find_package(Boost)
