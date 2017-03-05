f5a8cfda21f3f0ac89a39743981f3ff10a07bc3c
Sidney Niu <mingweiniu@gmail.com>
2017-03-05 17:05:06 +0800

    Update getBoost.cmake

--- cmake/Modules/getBoost.cmake
+++ cmake/Modules/getBoost.cmake
@@ -7,7 +7,7 @@ SET(Boost_USE_STATIC_LIBS ON)
 SET(Boost_USE_STATIC_RUNTIME ON)
 
 
-if(WiN32)
+if(WIN32)
 	add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
 	find_path(BOOST_ROOT NAMES boost/version.hpp HINTS
 		"$ENV{BOOST_ROOT}"
